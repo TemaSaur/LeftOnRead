4e2dd28650bd869247bdb973e4435fab7c860db2
unknown <SU@MR32-245-W18.at.urfu.ru>
2022-04-23 12:59:56 +0500

    commit

--- LeftOnRead/MainWindow.xaml.cs
+++ LeftOnRead/MainWindow.xaml.cs
@@ -12,17 +12,65 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace LeftOnRead
 {
 	/// <summary>
-	/// Логика взаимодействия для MainWindow.xaml
+	/// game logic
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		DispatcherTimer timer = new DispatcherTimer();
+		int gravity = 9;
+		Rect box;
+
 		public MainWindow()
 		{
 			InitializeComponent();
+			timer.Tick += MainEventTimer;
+			timer.Interval = TimeSpan.FromMilliseconds(20);
+
+			StartGame();
+		}
+
+
+		private void MainEventTimer(object sender, EventArgs args) {
+			box = new Rect(Canvas.GetLeft(HAHAHA), Canvas.GetTop(HAHAHA), HAHAHA.Width, HAHAHA.Height);
+
+			Canvas.SetTop(HAHAHA, Canvas.GetTop(HAHAHA) + gravity);
+		}
+
+		private void StartGame()
+		{
+			MainCanvas.Focus();
+
+			Canvas.SetTop(HAHAHA, 100);
+
+			foreach (var x in MainCanvas.Children.OfType<Border>())
+			{
+				if ((string)x.Tag == "Platform")
+				{
+					Canvas.SetLeft(x, 62);
+				}
+			}
+
+			timer.Start();
+		}
+
+		private void KeyIsUp(object sender, KeyEventArgs e)
+		{
+			HAHAHA.RenderTransform = new RotateTransform(5, HAHAHA.Width / 2, HAHAHA.Height / 2);
+			gravity = 9;
+		}
+
+		private void KeyIsDown(object sender, KeyEventArgs e)
+		{
+			if (e.Key == Key.Space)
+			{
+				HAHAHA.RenderTransform = new RotateTransform(-20, HAHAHA.Width/2, HAHAHA.Height/2);
+				gravity = -9;
+			}
 		}
 	}
 }
